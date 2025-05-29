cec8a6951fc767ec81c6f36c9fd5ba843abb1644
DheerajPanyam45 <dheeraj.panyam@gmail.com>
2025-05-29 06:14:46 +0530

    some part done in To Do

---
+++ ToDo/script.js
@@ -0,0 +1,21 @@
+let items = [];
+const itemsDiv = document.getElementById("items")
+
+function loadItems(){
+itemsDiv.innerHTML = null;
+for(const[idx,item] of Object.entries(items)){
+    
+}
+}
+function renderItems(){
+
+}
+function saveItems(){
+
+}
+function addItem(){
+
+}
+function removeItem(){
+
+}
